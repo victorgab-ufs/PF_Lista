b67cdf6e565dbdec67214620f8eb22f295caeccc
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-06-15 17:22:21 -0300

    Questão_2.js

--- Questão_2.js
+++ Questão_2.js
@@ -1,13 +1,18 @@
 //Q2. Programa para calcular a área de uma circunferência dado o valor do raio
 
-// FUNCTION
+// Com function:
 
-//function areaCircunferencia(raio){
-//const pi = 3.14
-//return pi*raio*raio
-//}
+function areaCircunferencia(raio){
+const pi = 3.14
+return pi*raio*raio
+}
 
-// ARROW
+// Arrow:
 
 const areaCirculo = (raio, pi=3.14) => pi*raio**2
+
+// Função Anônima: 
+
+const areaCirculo = (funcao, raio, pi=3.14) => funcao(raio, pi)
+const resultado = areaCirculo((raio, pi) => pi*raio*raio, 10)
 
