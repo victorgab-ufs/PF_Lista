2d34b786caab688d19b7e5d4f9b2ed6a825644f3
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-06-15 18:56:00 -0300

    Questão_3.js

--- Questão_3.js
+++ Questão_3.js
@@ -1,13 +1,13 @@
 //Q3. Programa para determinar se três valores passados podem representar um triângulo ou não.
 
-//FUNCTION
+// Com function: 
 
-//function existeTriangulo(lado1, lado2, lado3){
-//if (lado1+lado2>lado3 && lado1+lado3>lado2 && lado3+lado2>lado1) return 'É triângulo.'
-//else return 'Não é triângulo.'
-//}
+function existeTriangulo(lado1, lado2, lado3){
+if (lado1+lado2>lado3 && lado1+lado3>lado2 && lado3+lado2>lado1) return 'É triângulo.'
+else return 'Não é triângulo.'
+}
 
-//ARROW
+// Arrow:
 
 const existeTriangulo = (lado1, lado2, lado3) => lado1 && lado2 && lado3
 const lado1 = 1
@@ -18,6 +18,22 @@ const comparacao2 = lado1+lado3>lado2
 const comparacao3 = lado2+lado3>lado1
 console.log(existeTriangulo(comparacao1, comparacao2, comparacao3))
 
+// Função anônima:
+
+const existeTriangulo = (funcao, lado1, lado2, lado3) => funcao(lado1, lado2, lado3)
+
+const lado1 = 2
+const lado2 = 1
+const lado3 = 4
+
+const comparacao1 = lado1+lado2>lado3
+const comparacao2 = lado1+lado3>lado2
+const comparacao3 = lado2+lado3>lado1
+
+const conclusao = existeTriangulo((lado1, lado2, lado3) => (comparacao1 && comparacao2 && comparacao3), lado1, lado2, lado3)
+
+const resposta = (conclusao===true) ? 'É Triângulo' : 'Não é Triângulo'
+console.log(resposta)
 
 
 
