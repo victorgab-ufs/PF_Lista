99935accd82a07232fde7bb9bfc1e505cb21eddb
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-06-16 00:12:25 -0300

    Questão_14.js

--- Questão_14.js
+++ Questão_14.js
@@ -1,5 +1,22 @@
 //Q14. Escrever assunto por extenso
 
+// Com function:
+
+function algarismoPorExtenso(algarismo){
+if (algarismo === 0) return 'zero'
+if (algarismo === 1) return 'um'
+if (algarismo === 2) return 'dois'
+if (algarismo === 3) return 'tres'
+if (algarismo === 4) return 'quatro'
+if (algarismo === 5) return 'cinco'
+if (algarismo === 6) return 'seis'
+if (algarismo === 7) return 'sete'
+if (algarismo === 8) return 'oito'
+if (algarismo === 9) return 'nove'
+}
+
+// Arrow:
+
 const algarismoPorExtenso = (algarismo) => {
 if (algarismo === 0) return 'zero'
 if (algarismo === 1) return 'um'
@@ -13,4 +30,24 @@ if (algarismo === 8) return 'oito'
 if (algarismo === 9) return 'nove'
 }
 
-console.log(algarismoPorExtenso(2))
+// Anônima:
+
+const algarismoPorExtenso = (funcao, algarismo) => funcao(algarismo)
+
+const algarismos = (algarismo) => {
+
+if (algarismo === 0) return 'zero'
+if (algarismo === 1) return 'um'
+if (algarismo === 2) return 'dois'
+if (algarismo === 3) return 'tres'
+if (algarismo === 4) return 'quatro'
+if (algarismo === 5) return 'cinco'
+if (algarismo === 6) return 'seis'
+if (algarismo === 7) return 'sete'
+if (algarismo === 8) return 'oito'
+if (algarismo === 9) return 'nove'
+
+}
+
+const resultado = algarismos(algarismoPorExtenso(numero => numero, 2))
+
