eb9fd626a9bfd01ea5762d15ab23554f77bf7d5a
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-06-16 10:31:02 -0300

    Questão_5.js

--- Questão_5.js
+++ Questão_5.js
@@ -1,10 +1,14 @@
 //Q5. Distância Euclidiana
 
-//function distanciaEuclidianaEntreDoisPontos(x1,y1,x2,y2){
-//const a = y1-y2
-//const b = x1-x2
-//return ((a*a)+(b*b))**0.5
-//}
+// Com function:
+
+function distanciaEuclidianaEntreDoisPontos(x1,y1,x2,y2){
+const a = y1-y2
+const b = x1-x2
+return ((a*a)+(b*b))**0.5
+}
+
+// Arrow:
 
 const distanciaEntreDoisPontos = (deltaX, deltaY) => ((deltaX**2)+(deltaY**2))**0.5
 const x1 = 0
@@ -15,3 +19,10 @@ const deltaX = x1 - x2
 const deltaY = y1 - y2
 const resultadoDistancia = distanciaEntreDoisPontos(deltaX, deltaY)
 
+// Anônima:
+
+const distanciaEntreDoisPontos = (funcao, x1, y1, x2, y2) => funcao(x1, y1, x2, y2)
+
+const resultadoDistancia = distanciaEntreDoisPontos((x1, y1, x2, y2) => (((x1-x2)**2) + ((y1-y2)**2))**0.5, 1, 2, 1, 2)
+
+console.log(resultadoDistancia)
