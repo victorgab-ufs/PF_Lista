e8634392396cbb430d1902be6f67857204a00de9
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-06-16 09:43:53 -0300

    Questão_9.js

--- Questão_9.js
+++ Questão_9.js
@@ -1,11 +1,15 @@
 //Q9. OU Exclusivo
 
-//function calcularOuExclusivo(proposicaoA, proposicaoB){
-//const AouB = proposicaoA || proposicaoB
-//const negacaoAeB = !(proposicaoA && proposicaoB)
-//const ouExclusivo = (AouB && negacaoAeB)
-//return ouExclusivo
-//}
+// Com function:
+
+function calcularOuExclusivo(proposicaoA, proposicaoB){
+const AouB = proposicaoA || proposicaoB
+const negacaoAeB = !(proposicaoA && proposicaoB)
+const ouExclusivo = (AouB && negacaoAeB)
+return ouExclusivo
+}
+
+// Arrow: 
 
 const ouExclusivo = (expressaoP, expressaoQ) => expressaoP && expressaoQ
 const afirmacaoA = true
@@ -14,5 +18,16 @@ const expressaoP = afirmacaoA || afirmacaoB
 const expressaoQ = !(afirmacaoA && afirmacaoB)
 const resultado = ouExclusivo(expressaoP, expressaoQ)
 
+// Anônima:
 
+const ouExclusivo = (funcao, expressaoP, expressaoQ) => funcao(expressaoP, expressaoQ)
 
+const afirmacaoA = true
+const afirmacaoB = true
+
+const expressaoP = afirmacaoA || afirmacaoB
+const expressaoQ = !(afirmacaoA && afirmacaoB)
+
+const resultado = ouExclusivo((afirmacaoA, afirmacaoB) => expressaoP && expressaoQ)
+
+console.log(resultado)
