b84852ab72ccb1476fde10c49e7697bbb4a1f87f
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-06-16 00:24:59 -0300

    Questão_13.js

--- Questão_13.js
+++ Questão_13.js
@@ -1,15 +1,25 @@
 //Q13. Velocidade (tempo/posição)
 
-//function velocidade(instanteT, posicaoX){
-//const posicao = posicaoX - 500
-//const tempo = instanteT - 0
-//const velocidadeMedia = posicao/tempo
-//return `${velocidadeMedia} m/s`
-//}
+// Com function:
+
+function velocidade(instanteT, posicaoX){
+const posicao = posicaoX - 500
+const tempo = instanteT - 0
+const velocidadeMedia = posicao/tempo
+return `${velocidadeMedia} m/s`
+}
+
+// Arrow:
 
 const velocidadeXnoPontoT = (posicao, tempo) => posicao/tempo
 const posicaoFinal = 1000
 const posicao = posicaoFinal - 500
 const tempo = 5
 const velocidade = velocidadeXnoPontoT(posicao, tempo)
+
+// Anônima:
+
+const velocidadePontual = (funcao, posicao, tempo) => funcao(posicao, tempo)
+const valorDaVelocidade = velocidadePontual((posicao, tempo) => (posicao-500)/(tempo-5), 1000, 10)
+
 
