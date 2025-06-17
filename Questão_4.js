d6391bb296d0def5ff2f1b6b5d9e99feaa8e22c9
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-06-16 22:38:05 -0300

    Questão_4.js

--- Questão_4.js
+++ Questão_4.js
@@ -1,11 +1,15 @@
 //Q4. Classifique o triângulo em escaleno, equilátero ou isósceles
 
-//function tipoTriangulo(lado1, lado2, lado3){
-//if (lado1===lado2 && lado2===lado3) console.log('equilatero')
-//else
-//if (lado1!==lado2 && lado1!==lado3 && lado2!==lado3) console.log('escaleno')
-//else console.log('isósceles')
-//}
+// Com function:
+
+function tipoTriangulo(lado1, lado2, lado3){
+if (lado1===lado2 && lado2===lado3) console.log('equilatero')
+else
+  if (lado1!==lado2 && lado1!==lado3 && lado2!==lado3) console.log('escaleno')
+  else console.log('isósceles')
+}
+
+// Arrow:
 
 const tipoTriangulo = (lado1, lado2, lado3) => {
 if (lado1===lado2 &&lado2===lado3) return 'Equilátero'
@@ -13,4 +17,20 @@ else
 if (lado1!==lado2 && lado2!==lado3 && lado1!==lado3) return "Escaleno"
 else return "Isósceles"
 }
+
+// Anônima:
+
+const tipoTriangulo = (funcao, lado1, lado2, lado3) => funcao(lado1, lado2, lado3)
+
+const resposta = tipoTriangulo((lado1, lado2, lado3) => {
+
+if (lado1===lado2 &&lado2===lado3) return 'Equilátero'
+else
+  if (lado1!==lado2 && lado2!==lado3 && lado1!==lado3) return "Escaleno"
+  else return "Isósceles"
+
+}, 1, 2, 3)
+
+
+
 
