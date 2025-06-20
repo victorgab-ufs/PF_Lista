cb445945230eafbf6a875666666124f8089e92be
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-06-19 22:59:37 -0300

    Questão_14.js

--- Questão_14.js
+++ Questão_14.js
@@ -51,3 +51,24 @@ if (algarismo === 9) return 'nove'
 
 const resultado = algarismos(algarismoPorExtenso(numero => numero, 2))
 
+// Currying:
+
+const algarismoPorExtenso = (algarismo) => {
+
+if (algarismo === 0) return 'zero'
+if (algarismo === 1) return 'um'
+if (algarismo === 2) return 'dois'
+if (algarismo === 3) return 'três'
+if (algarismo === 4) return 'quatro'
+if (algarismo === 5) return 'cinco'
+if (algarismo === 6) return 'seis'
+if (algarismo === 7) return 'sete'
+if (algarismo === 8) return 'oito'
+if (algarismo === 9) return 'nove'
+
+}
+
+const resultado = algarismoPorExtenso(5)
+
+
+
