81371633cde4fcaf161b1ff3964c30f45403e8f2
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-06-19 21:40:52 -0300

    Questão_09.js

--- Questão_09.js
+++ Questão_09.js
@@ -21,13 +21,18 @@ const resultado = ouExclusivo(expressaoP, expressaoQ)
 // Anônima:
 
 const ouExclusivo = (funcao, expressaoP, expressaoQ) => funcao(expressaoP, expressaoQ)
-
 const afirmacaoA = true
 const afirmacaoB = true
-
 const expressaoP = afirmacaoA || afirmacaoB
 const expressaoQ = !(afirmacaoA && afirmacaoB)
-
 const resultado = ouExclusivo((afirmacaoA, afirmacaoB) => expressaoP && expressaoQ)
 
-console.log(resultado)
+// Currying: 
+
+const ouExclusivo = (AouB) => (negacaoAeB) => (AouB && negacaoAeB)
+const AouB = (expressaoA) => (expressaoB) => (expressaoA || expressaoB)
+const negacaoAeB = (expressaoA) => (expressaoB) => !(expressaoA && expressaoB)
+const conclusaoA = false
+const conclusaoB = true
+const resultado = ouExclusivo(AouB(conclusaoA)(conclusaoB))(negacaoAeB(conclusaoA)(conclusaoB))
+
